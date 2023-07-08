cbe72a30976c9952925b9a0747bdcbdb07464e85
Octavian Nitu <nitutavi@gmail.com>
2023-07-08 21:16:07 +0300

    Open YouTube, play a video, rec video(only 12FPS), rec audio (only mic)

--- VideoYT.py
+++ VideoYT.py
@@ -8,14 +8,16 @@ import time
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 import requests
+import sounddevice as sd
+import soundfile as sf
 
 
 def conexiune():
-    try:
-        requests.get('https://google.com/')
+    if requests.get('https://google.com/'):
         return True
-    except:
+    else:
         return False
+
 
 class YouTube:
     def __init__(self):
@@ -26,17 +28,20 @@ class YouTube:
         self.browser.maximize_window()
         self.browser.implicitly_wait(120)
         try:
-            rej_cookies = self.browser.find_element(By.XPATH, "/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/div[4]/div[2]/div[6]/div[1]/ytd-button-renderer[1]/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]").click()
+            rej_cookies = self.browser.find_element(By.XPATH,
+                                                    "/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/div[4]/div[2]/div[6]/div[1]/ytd-button-renderer[1]/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]").click()
         except:
             print("Nu avem butonul Reject ALL")
 
         try:
-            video_rand = self.browser.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[6]/ytd-rich-grid-row[1]/div/ytd-rich-item-renderer[1]/div/ytd-rich-grid-media/div[1]/ytd-thumbnail/a/yt-image/img").click()
+            video_rand = self.browser.find_element(By.XPATH,
+                                                   "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[6]/ytd-rich-grid-row[1]/div/ytd-rich-item-renderer[1]/div/ytd-rich-grid-media/div[1]/ytd-thumbnail/a/yt-image/img").click()
         except:
             print("Nu ajunge pana in punctul de play")
 
         try:
-            skipButton = self.browser.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[18]/div/div[3]/div/div[2]/span/button/div")
+            skipButton = self.browser.find_element(By.XPATH,
+                                                   "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[18]/div/div[3]/div/div[2]/span/button/div")
             if skipButton:
                 skipButton.click()
         except:
@@ -50,13 +55,20 @@ class YouTube:
 
     def record_video(self):
 
+        # display screen resolution, get it using pyautogui itself
         SCREEN_SIZE = tuple(pyautogui.size())
+        # define the codec
         fourcc = cv2.VideoWriter_fourcc(*"XVID")
+        # frames per second
         fps = 12.0
         # create the video write object
-        out = cv2.VideoWriter("output.avi", fourcc, fps, (SCREEN_SIZE))
+        filename = "output.avi"
+        out = cv2.VideoWriter(filename, fourcc, fps, (SCREEN_SIZE))
         # the time you want to record in seconds
         record_seconds = 10
+
+        print("Recording video ...")
+
         for i in range(int(record_seconds * fps)):
             # make a screenshot
             img = pyautogui.screenshot()
@@ -66,21 +78,41 @@ class YouTube:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             # write the frame
             out.write(frame)
-            # if the user clicks q, it exits
-            if cv2.waitKey(1) == ord("q"):
-                break
+
+        print(f"Recording saved to {filename}")
+
         # make sure everything is closed when exited
-        ### cv2.destroyAllWindows()
+
+        cv2.destroyAllWindows()
         out.release()
+
+    def record_audio(self):
+
+        # Set the audio settings
+        sample_rate = 44100  # Sample rate in Hz
+        duration = 5  # Duration of the recording in seconds
+
+        # Record the audio
+        print("Recording audio ...")
+        audio = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1)
+
+        # Wait for the recording to complete
+        sd.wait()
+
+        # Save the audio to a file
+        filename = "audio.wav"
+        sf.write(filename, audio, sample_rate)
+
+        print(f"Recording saved to {filename}")
 
 
 if conexiune():
 
-
     site = YouTube()
     site.openYouTube()
-
-    input("Apasa ENTER pentru a inchide pagina")
+    site.record_video()
+    site.record_audio()
+    input("Nu mai exista instructiuni, apasa ENTER pentru a inchide pagina")
 
 else:
     print("Nu se conecteaza la Internet")
